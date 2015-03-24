eb80e0d16f5af616c9edd040370bd59512c5a883
Paul Marganian <pmargani@trent.(none)>
2015-03-24 08:14:40 -0400

    Added Django-ExtJs-Mashup dir and it's contents: needed for Djanog1.6

---
+++ Django-ExtJS-Mashup/urls.py
@@ -0,0 +1,32 @@
+#from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+#from polls.views     import *
+#from polls.resources import *
+
+#pollResource = PollResource()
+#print pollResource
+
+from polls import *
+
+urlpatterns = patterns('',
+    (r'polls/',  include('polls.urls')), 
+    #(r'^static/(?P<path>.*)$', 'django.views.static.serve'
+    #, {'document_root': settings.STATIC_POLLS}),
+    #url(r'^$',                       root),
+    #url(r'^polls/([^/]+)$',          pollResource.requestHandler), 
+    #url(r'^polls$',                  pollResource.requestHandler),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
